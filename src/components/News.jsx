6efa69927ccf2fc67d075d64c378a61cd3416b86
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-06 19:22:58 +0800

    fetch and display news data

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
 
-const News = () => {
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import demoImage from '../images/news.jpg';
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: "Cryptocurrency", count: simplified ? 6 : 50 });
+
+  console.log(cryptoNews);
+
+  if (!cryptoNews?.value) return "Loading...";
+
   return (
-    <div>News</div>
+    <Row gutter={[24,24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news=card">
+            <a href={news.url} target="_blank" rel="noreferrer noopener">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>{news.name}</Title>
+                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" style={{ maxWidth: '200px', maxHeight: '100px' }} />
+              </div>
+              <p>
+                {news.description > 100 
+                ? `${news.description.substring(0, 100)} ...` 
+                : news.description } 
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news source" />
+                  <Text className="provider-name">{news.provider[0]?.name}</Text>
+                </div>
+                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   )
-}
+};
 
-export default News
+export default News;
