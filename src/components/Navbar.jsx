0c5172c0832a52040f5a1f5f05fcf8347153c0ac
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-06 20:41:02 +0800

    use menu items antd as children soon deprecated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,30 @@ import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOut
 import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
+
+    const menuItems = [
+        {
+            key: 'home',
+            icon: <HomeOutlined />,
+            label: <Link to="/">Home</Link>
+        },
+        {
+            key: 'cryptocurrencies',
+            icon: <FundOutlined />,
+            label: <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        },
+        {
+            key: 'exchanges',
+            icon: <MoneyCollectOutlined />,
+            label: <Link to="/exchanges">Exchanges</Link>
+        },
+        {
+            key: 'news',
+            icon: <BulbOutlined />,
+            label: <Link to="/news">News</Link>
+        },
+    ];
+
   return (
     <div className="nav-container">
         <div className="logo-container">
@@ -16,20 +40,7 @@ const Navbar = () => {
 
             </Button>
         </div>
-        <Menu theme="dark">
-            <Menu.Item icon={<HomeOutlined />}>
-                <Link to="/">Home</Link>
-            </Menu.Item>
-            <Menu.Item icon={<FundOutlined />}>
-                <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-            </Menu.Item>
-            <Menu.Item icon={<MoneyCollectOutlined />}>
-                <Link to="/exchanges">Exchanges</Link>
-            </Menu.Item>
-            <Menu.Item icon={<BulbOutlined />}>
-                <Link to="/news">News</Link>
-            </Menu.Item>
-        </Menu>
+        <Menu theme="dark" items={menuItems} />
     </div>
   )
 }
