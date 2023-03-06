efd3c8757d71728da7dc4cab0fbbff68b1a1c652
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-07 07:56:21 +0800

    add mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 import icon from '../images/cryptocurrency.png'
 
 const Navbar = () => {
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [screenSize, setScreenSize] = useState(null)
+
+    useEffect(() => {
+      const handleResize = () => setScreenSize(window.innerWidth);
+      window.addEventListener('resize', handleResize);
+      handleResize();
+      return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+    useEffect(() => {
+      if (screenSize < 768) { 
+        setActiveMenu(false);
+    } else {
+        setActiveMenu(true);
+      }
+    }, [screenSize])
+    
+    
 
     const menuItems = [
         {
@@ -36,11 +55,13 @@ const Navbar = () => {
             <Typography.Title level={2} className="logo">
                 <Link to="/">Cryptoverse</Link>
             </Typography.Title>
-            <Button className="menu-control-container">
-
+            <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
+                <MenuOutlined />
             </Button>
         </div>
-        <Menu theme="dark" items={menuItems} />
+        {activeMenu && (
+            <Menu theme="dark" items={menuItems} />
+        )}
     </div>
   )
 }
