49a13fdcc419d5ffe3b49f23b91ff3746d6ea8f3
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-06 17:36:50 +0800

    implement homepage with dummy data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Title } = Typography;
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+    <Title level={2} className="heading">Global Crypto Stats</Title>
+    <Row>
+      <Col span={12}><Statistic title="Total Cryptocurrencies" value="1337" /></Col>
+      <Col span={12}><Statistic title="Total Exchanges" value="57" /></Col>
+      <Col span={12}><Statistic title="Total Market Cap" value="$1,234,567,890,987" /></Col>
+      <Col span={12}><Statistic title="Total 24h Volume" value="$7,777,777,777" /></Col>
+      <Col span={12}><Statistic title="Total Markets" value="5555" /></Col>
+    </Row>
+    </>
   )
 }
 
