a74a1b423223e9ea0d8ed45379e2212339c95247
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-06 21:37:39 +0800

    show exchanges of btc market

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from 'react';
+import millify from 'millify';
+import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import HTMLReactParser from 'html-react-parser';
+
+import { useGetExchangesQuery } from '../services/cryptoApi';
+// import Loader from './Loader';
+
+const { Text } = Typography;
+const { Panel } = Collapse;
 
 const Exchanges = () => {
-  return (
-    <div>Exchanges</div>
-  )
-}
+  const { data, isFetching } = useGetExchangesQuery();
+  const exchangesList = data?.data?.exchanges;
+  const stats = data?.data?.stats;
 
-export default Exchanges
+  // if (isFetching) return <Loader />;
+  if (isFetching) return "Loading...";
+
+  return (
+    <>
+      <Row>
+        <Col span={6}>Exchanges</Col>
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Share of Total</Col>
+      </Row>
+      <Row>
+        {exchangesList.map((exchange) => (
+          <Col span={24} key={exchange.uuid}>
+            <Collapse>
+              <Panel
+                key={exchange.uuid}
+                showArrow={false}
+                header={(
+                  <Row>
+                    <Col span={6}>
+                      <Text><strong>{exchange.rank}.</strong></Text>
+                      <Avatar className="exchange-image" src={exchange.iconUrl} />
+                      <Text><strong>{exchange.name}</strong></Text>
+                    </Col>
+                    <Col span={6}>${millify(exchange['24hVolume'])}</Col>
+                    <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+                    <Col span={6}>{millify(exchange['24hVolume'] / stats['24hVolume'] * 100 )}%</Col>
+                  </Row>
+                  )}
+              >
+                {HTMLReactParser(exchange.coinrankingUrl || 'No description.')}
+              </Panel>
+            </Collapse>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default Exchanges;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -22,6 +22,9 @@ export const cryptoApi = createApi({
         getCryptoHistory: builder.query({
             query: ({coinId, timePeriod}) => createRequest(`/coin/${coinId}/history?timePeriod=${timePeriod}`)
         }),
+        getExchanges: builder.query({
+            query: () => createRequest(`/coin/Qwsogvtv82FCd/exchanges?limit=100`)
+        }),
     })
 })
 
@@ -29,4 +32,5 @@ export const {
     useGetCryptosQuery,
     useGetCryptoDetailsQuery,
     useGetCryptoHistoryQuery,
+    useGetExchangesQuery,
 } = cryptoApi;
